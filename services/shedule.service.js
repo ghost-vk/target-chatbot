8cec4e26fdb620935fab1bcabcbbe28eb6553012
ghost <ghost.vkv@gmail.com>
2022-02-21 15:30:21 +0300

    Remove handling subscriptions

--- services/shedule.service.js
+++ services/shedule.service.js
@@ -1,18 +1,19 @@
 const schedule = require('node-schedule')
 const CurrencyService = require('./currency.service')
 const { GHOST_ID } = require('./../config')
-const SubscriptionService = require('./subscription.service')
 const { sendMessages } = require('./bot.service')
 const debug = require('debug')('service:schedule')
+// const SubscriptionService = require('./subscription.service')
 
 module.exports = function scheduleWork() {
   schedule.scheduleJob('0 12 * * *', async () => {
     try {
       debug('Schedule job starts!')
-      // Update currency
+
       await CurrencyService.updateCurrency()
-      // Handle subscriptions
-      await SubscriptionService.handleSubscriptions()
+
+      // await SubscriptionService.handleSubscriptions()
+
       await sendMessages({
         type: 'message',
         chatId: GHOST_ID,
@@ -20,10 +21,13 @@ module.exports = function scheduleWork() {
       })
     } catch (e) {
       let text = 'Ошибка при выполнении ежедневных работ'
+
       if (e.message) {
         text += `: ${e}`
       }
+
       await sendMessages({ type: 'message', chatId: GHOST_ID, text })
+
       throw new Error(e)
     }
   })
